d1e20835eabc1b9054707d3ec2fd7ef3cc59ff2b
tejashcrest <tejash.p@crestinfosystems.com>
2021-06-03 18:36:22 +0530

    Add files via upload

--- src/components/common/PaymentIcon.js
+++ src/components/common/PaymentIcon.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const PaymentIcon = (props) =>
   <>
-    {props.images.map(image => (
-      <div className={"col-xl-4 col-lg-4 col-md-6 col-sm-6"} ><div className={"img-wrapper mb-30 p-4"}><img className={"payment-method"} src={`../../assets/images/payment/logos/${image.name}`} alt="" /></div></div>
+    {props.images.map((image, index) => (
+      <div key={index} className={"col-xl-4 col-lg-4 col-md-6 col-sm-6"} ><div className={"img-wrapper mb-30 p-4"}><img className={"payment-method"} src={`../../assets/images/payment/logos/${image.name}`} alt="" /></div></div>
     ))}
   </>
 
